b0646ec276759bd6061df74dd77137b8924dd6c4
Kyosuke Noguchi <kn1359028527@gmail.com>
2026-02-09 00:55:11 +0900

    コンポーネント用の色味を追加

--- Color-Checker/Color+Type.swift
+++ Color-Checker/Color+Type.swift
@@ -2,13 +2,19 @@ import Foundation
 import SwiftUI
 
 struct Component{
+    let borderColor:Color
+    let compBackground:Color
+    let compSelectBackground:Color
     let colorSelectButtonBackgrond:Color
     let buttonBackground:Color
     let buttonBlueBackground:Color
     let buttonPurpleBackground:Color
     let buttonBorder:Color
     let textFieldBorder:Color
+    let selectedTabColor:Color
+    let defaultTabColor:Color
 }
+
 
 struct Base{
     let BaseBackground:Color
@@ -38,12 +44,17 @@ extension Color{
             blueFontColor: Color.rgba(r: 132, g: 137, b: 221),
         ),
         component:Component(
+            borderColor:Color.rgba(r: 65, g: 61, b: 115),
+            compBackground:Color.rgba(r: 26, g: 21, b: 59),
+            compSelectBackground:Color.rgba(r: 26, g: 21, b: 59,a:0.8),
             colorSelectButtonBackgrond: Color.rgba(r: 46, g: 72, b: 160),
             buttonBackground: Color.rgba(r: 40, g: 42, b: 65),
             buttonBlueBackground: Color.rgba(r: 46, g: 72, b: 160),
             buttonPurpleBackground: Color.rgba(r: 88, g: 82, b: 228),
             buttonBorder: Color.rgba(r: 159, g: 167, b: 202),
             textFieldBorder: Color.rgba(r: 186, g: 197, b: 228),
+            selectedTabColor:Color.rgba(r: 152, g: 173, b: 253),
+            defaultTabColor:Color.rgba(r: 105, g: 94, b: 154)
         ),
     )
    
